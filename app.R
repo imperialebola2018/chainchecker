576ad22231805519eda3041ac3a1cc0b3449b1aa
katy <k.gaythorpe@imperial.ac.uk>
2018-11-01 15:56:31 +0000

    Initial commit

---
+++ app.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Calculate onset date"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      dateInput("death",
+                "Date of death:",
+                value = Sys.Date()),
+      numericInput("symptomatic",
+                   "Duration of symptomatic period:",
+                   value = 7),
+      numericInput("incubation",
+                   "Duration of incubation period:",
+                   value = 21),
+      textInput("id",
+                "Identifyer:",
+                value = "ID1")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("onsetPlot")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$onsetPlot <- renderPlot({
+    
+    df = data.frame("ID" = input$id,
+                    "Death" = input$death,
+                    "Symptomatic" = input$death - input$symptomatic,
+                    "Exposure" = input$death - input$symptomatic- input$incubation)
+    
+    g = ggplot(df) 
+    g = g + geom_rect(aes(xmin = Exposure,
+                          xmax = Symptomatic,
+                          ymin = ID, 
+                          ymax = ID))
+    g
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
