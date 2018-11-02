9382a15f58e51e0e93da3debe5dc357812042536
katy <k.gaythorpe@imperial.ac.uk>
2018-11-02 12:50:12 +0000

    Added new input panel- nothing works on the figures as yet

--- ui.R
+++ ui.R
@@ -3,26 +3,63 @@ library(shiny)
 fluidPage(
   
   # Application title
-  titlePanel("Calculate onset date"),
+  titlePanel("Calculating Ebola exposure dates"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      dateInput("death",
-                "Date of death:",
-                value = Sys.Date()),
-      dateInput("report_onset",
-                "Reported onset date:",
-                value = (Sys.Date()-7)),
-      numericInput("symptomatic",
-                   "Duration of symptomatic period:",
-                   value = 7),
-      numericInput("incubation",
-                   "Duration of incubation period:",
+      
+      #standard inputs
+      numericInput("min_incubation",
+                   "Duration of incubation period min:",
                    value = 21),
+      numericInput("max_incubation",
+                   "Duration of incubation period max:",
+                   value = 21),
+      numericInput("symptomatic",
+                   "Duration of period from onset to death:",
+                   value = 7),
+      
       textInput("id",
                 "Identifier:",
-                value = "ID1")
+                value = "ID1"),
+      
+      #conditions
+      checkboxInput("death_avail", "Check box if the date of death is available", value = TRUE), 
+      conditionalPanel(
+        condition = "input.death_avail == true",
+        dateInput("death",
+                  "Date of death:",
+                  value = Sys.Date())
+      ), 
+      conditionalPanel(
+        condition = "input.death_avail == false",
+        dateInput("report_onset",
+                  "Reported onset date:",
+                  value = (Sys.Date()-7)),
+        checkboxInput("bleeding", "Check box if the individual was bleeding*.", value = TRUE),
+        conditionalPanel(
+          condition = "input.bleeding == true",
+          numericInput("bleeding_correction",
+                       "Estimate of time from onset to bleeding:",
+                       value = 4)
+        ),
+        conditionalPanel(
+          condition = "input.bleeding == false",
+          checkboxInput("diarrhea", "Check box if the individual had diarrhea."),
+          conditionalPanel(
+            condition = "input.diarrhea == true",
+            numericInput("diarrhea_correction",
+                         "Estimate of time from onset to diarrhea:",
+                         value = 4)
+          )
+          
+        )
+        
+        
+      )
+      
+      
     ),
     
     # Show a plot of the generated distribution
