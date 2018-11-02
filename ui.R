f4b8416fb7e9d3598819c0fb09e7323bb1b098bf
katy <k.gaythorpe@imperial.ac.uk>
2018-11-02 15:06:09 +0000

    moved tabs around to be pages with separate inputs

--- ui.R
+++ ui.R
@@ -1,88 +1,110 @@
 library(shiny)
 # Define UI for application that draws a histogram
-fluidPage(
-  
-  # Application title
-  titlePanel("Calculating Ebola exposure dates"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      
-      #standard inputs
-      numericInput("min_incubation",
-                   "Duration of incubation period min:",
-                   value = 14),
-      numericInput("max_incubation",
-                   "Duration of incubation period max:",
-                   value = 21),
-      numericInput("symptomatic",
-                   "Duration of period from onset to death:",
-                   value = 9),
-      
-      textInput("id",
-                "Identifier:",
-                value = "ID1"),
-      
-      #conditions
-      checkboxInput("death_avail", "Check box if the date of death is available", value = TRUE), 
-      conditionalPanel(
-        condition = "input.death_avail == true",
-        dateInput("death",
-                  "Date of death:",
-                  value = Sys.Date())
-      ), 
-      conditionalPanel(
-        condition = "input.death_avail == false",
-        dateInput("report_onset",
-                  "Reported onset date:",
-                  value = Sys.Date()-7),
-        checkboxInput("bleeding", "Check box if the individual was bleeding*.", value = TRUE),
-        conditionalPanel(
-          condition = "input.bleeding == true",
-          numericInput("bleeding_correction",
-                       "Estimate of time from onset to bleeding:",
-                       value = 6)
-        ),
-        conditionalPanel(
-          condition = "input.bleeding == false",
-          checkboxInput("diarrhea", "Check box if the individual had diarrhea."),
-          conditionalPanel(
-            condition = "input.diarrhea == true",
-            numericInput("diarrhea_correction",
-                         "Estimate of time from onset to diarrhea:",
-                         value = 4)
-          )
-          
-        )
-        
-        
-      )
-      
-      
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(type = "tabs",
-                  tabPanel("Timeline", 
-                           plotOutput("exposure_plot"),
-                           textOutput("estimated_onset"),
-                           textOutput("exposure_window")),
-                  # tabPanel("Probability", 
-                  #          textOutput("prob_intro"),
-                  #          plotOutput("dist_plot"),
-                  #          textOutput("prob_onset")),
-                  tabPanel("Upload",
-                           downloadButton("download_ctemplate", "Download contacts template"),
-                           downloadButton("download_ltemplate", "Download linelist template"),
-                           fileInput("file_line", h3("Upload linelist")),
-                           fileInput("file_contact", h3("Upload contacts"))),
-                  tabPanel("Analysis",
-                           plotOutput("death_onset_plot")
-                  )
-      )
-      
-    )
-  )
+navbarPage("Calculating Ebola exposure dates", 
+           selected = "Timeline",
+           
+           
+           #making sure the figue is not huge
+           tags$head(tags$style(
+             type="text/css",
+             "#Diagram img {max-height: 100%; height: 100%; width: auto}"
+           )),
+           
+           # Sidebar with a slider input for number of bins 
+           tabPanel("Timeline",
+                    sidebarPanel(
+                      
+                      #standard inputs
+                      numericInput("min_incubation",
+                                   "Duration of incubation period min:",
+                                   value = 14),
+                      numericInput("max_incubation",
+                                   "Duration of incubation period max:",
+                                   value = 21),
+                      numericInput("symptomatic",
+                                   "Duration of period from onset to death:",
+                                   value = 9),
+                      
+                      textInput("id",
+                                "Identifier:",
+                                value = "ID1"),
+                      
+                      #conditions
+                      checkboxInput("death_avail", "Check box if the date of death is available", value = TRUE), 
+                      conditionalPanel(
+                        condition = "input.death_avail == true",
+                        dateInput("death",
+                                  "Date of death:",
+                                  value = Sys.Date())
+                      ), 
+                      conditionalPanel(
+                        condition = "input.death_avail == false",
+                        dateInput("report_onset",
+                                  "Reported onset date:",
+                                  value = Sys.Date()-7),
+                        checkboxInput("bleeding", "Check box if the individual was bleeding*.", value = TRUE),
+                        conditionalPanel(
+                          condition = "input.bleeding == true",
+                          numericInput("bleeding_correction",
+                                       "Estimate of time from onset to bleeding:",
+                                       value = 6)
+                        ),
+                        conditionalPanel(
+                          condition = "input.bleeding == false",
+                          checkboxInput("diarrhea", "Check box if the individual had diarrhea."),
+                          conditionalPanel(
+                            condition = "input.diarrhea == true",
+                            numericInput("diarrhea_correction",
+                                         "Estimate of time from onset to diarrhea:",
+                                         value = 4)
+                          )
+                        )
+                      )
+                      
+                      
+                    ),
+                    mainPanel(plotOutput("exposure_plot"),
+                              textOutput("estimated_onset"),
+                              textOutput("exposure_window"))
+           ),
+           tabPanel("Upload",
+                    downloadButton("download_ctemplate", "Download contacts template"),
+                    downloadButton("download_ltemplate", "Download linelist template"),
+                    fileInput("file_line", h3("Upload linelist")),
+                    fileInput("file_contact", h3("Upload contacts"))
+           ),
+           tabPanel("Analysis of upload",
+                    
+                    sidebarPanel(
+                      
+                      #standard inputs
+                      numericInput("min_incubation",
+                                   "Duration of incubation period min:",
+                                   value = 14),
+                      numericInput("max_incubation",
+                                   "Duration of incubation period max:",
+                                   value = 21),
+                      numericInput("symptomatic",
+                                   "Duration of period from onset to death:",
+                                   value = 9),
+                      numericInput("bleeding_correction",
+                                   "Estimate of time from onset to bleeding:",
+                                   value = 6),
+                      numericInput("diarrhea_correction",
+                                   "Estimate of time from onset to diarrhea:",
+                                   value = 4)
+
+                    ),
+                    mainPanel(plotOutput("onset_plot"))
+           ),
+           tabPanel("Method and definitions",
+                    "We make certain assumptions about how disease prgresses from one stage to the next. 
+                           We assume individuals are exposed, become infectious with some symptoms and then, 
+                           in some cases, die. The different stages and definitions are shown below.",
+                    br(),
+                    imageOutput("Diagram"),
+                    br(),
+                    "*Bleeding is defined as ..."
+           )
+           
 )
