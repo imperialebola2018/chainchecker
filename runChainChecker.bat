7832ea306e69f9070418fa9f57fc65240672aa05
Miles Stewart <Miles.Stewart@jhuapl.edu>
2019-03-08 17:16:11 -0500

    Adding files for running portable distribution of chain checker

---
+++ runChainChecker.bat
@@ -0,0 +1,3 @@
+SET R0PTS=--no-save --no-environ --no-init-file --no-restore --no-Rconsole
+
+R-Portable\App\R-Portable\bin\Rscript.exe %R0PTS% runServer.R 1> ShinyApp.log 2>&1
---
+++ runServer.R
@@ -0,0 +1,4 @@
+options(shiny.port=8080)
+browseURL("http://localhost:8080")
+shiny::runApp('.')
+
